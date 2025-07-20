565aa969b7fb82296899d9d1d4dd6ed9bd1dc2f1
kuroi <Kuroiryujin1@gmail.com>
2025-07-20 13:40:21 +0200

    Remove debug output from Program.cs
    Removed unnecessary debug `Console.WriteLine` statements that printed the target number and user input. This cleanup ensures the main program logic focuses only on the intended user interactions.

--- Plus or minus/Program.cs
+++ Plus or minus/Program.cs
@@ -6,11 +6,9 @@ Console.WriteLine("Game start");
 // Generate a random integer between x and y which is the target number
 var randomInstance = new Random();
 var targetNumber = randomInstance.Next(1, 101);
-Console.WriteLine(targetNumber);
 // Ask user to input a number
 Console.Write("Input a number: ");
 var userInput = Console.ReadLine();
-Console.WriteLine(userInput);
 // Compare user input and target number
 // Tell user if the input number is higher or lower than target number
 // Ask user to input a number again, repeat until numbers match
