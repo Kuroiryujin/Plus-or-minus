01389d767ab7094c962c495d7a3b90283d40165f
kuroi <Kuroiryujin1@gmail.com>
2025-07-20 14:09:10 +0200

    Refactor gameplay loop to allow repeated user inputs
    Changed the program to use a loop, letting the user guess multiple times until the correct number is entered. This replaces the single input approach, improving usability and aligning with typical number-guessing game mechanics.

--- Plus or minus/Program.cs
+++ Plus or minus/Program.cs
@@ -4,24 +4,34 @@ Console.WriteLine("Game start");
 
 
 // Generate a random integer between x and y which is the target number
+
 var randomInstance = new Random();
 var targetNumber = randomInstance.Next(1, 101);
+
 // Ask user to input a number
-Console.Write("Input a number: ");
-var userInput = Console.ReadLine();
-// Compare user input and target number
-// Tell user if the input number is higher or lower than target number
-if (targetNumber > int.Parse(userInput))
-{
-    Console.WriteLine("Too high");
-}
-else if (targetNumber < int.Parse(userInput))
-{
-    Console.WriteLine("Too low");
-}
-else
-{
-    Console.WriteLine("You win!");
-}
 // Ask user to input a number again, repeat until numbers match
-// Winning message when the numbers are equal
+var isContinue = true;
+
+do
+{
+    Console.Write("Input a number: ");
+    var userInput = int.Parse(Console.ReadLine());
+
+    // Compare user input and target number
+    // Tell user if the input number is higher or lower than target number
+    // Winning message when the numbers are equal
+
+    if (targetNumber > userInput)
+    {
+        Console.WriteLine("Too low");
+    }
+    else if (targetNumber < userInput)
+    {
+        Console.WriteLine("Too high");
+    }
+    else
+    {
+        Console.WriteLine("You win!");
+        isContinue = false;
+    }
+} while (isContinue);
